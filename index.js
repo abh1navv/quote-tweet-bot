1bb9d8cab1dd6a6e51f97621a0b3b252c1932ace
Abhinav Pandey <abhinavpandey312@gmail.com>
2021-09-05 12:54:40 +0000

    Retweets best tweets and works for multiple users

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const Twitter = require('twitter-v2');
 
-const client = new Twitter({
+const TwitterLite = require('twitter-lite');
+
+const getClient = new Twitter({
+  bearer_token: process.env.BEARER_TOKEN,
+});
+
+const postClient = new TwitterLite({  
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_SECRET,
   consumer_key: process.env.CONS_KEY,
-  consumer_secret: process.env.CONS_SECRET,
-  //access_token_key: process.env.ACCESS_TOKEN,
-  //access_token_secret: process.env.ACCESS_SECRET,
-  bearer_token:process.env.BEARER_TOKEN
+  consumer_secret: process.env.CONS_SECRET
 });
 
 
@@ -24,19 +29,46 @@ function getBestTweet(data) {
   return bestTweetId;
 }
 
+function getFromClauses() {
+  let fromClause = "";
+  const userList = process.env.USER_LIST;
+  const users = userList.split(",");
+  for(const user in users) {
+    fromClause += 'OR '+ 'from:' + users[user] + ' ';
+  }
+  fromClause = fromClause.substring(3);
+  return fromClause;
+}
+
 async function main() {
   let yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1)
-  let params = {};
-  params.query = 'from:dthompsondev -is:reply -is:retweet';
-  params.tweet = {};
-  params.tweet.fields = 'public_metrics';
-  const {data} = await client.get('tweets/search/recent' ,params);
-  console.log(data);
+  yesterday.setDate(yesterday.getDate())
+  yesterday.setHours((yesterday.getHours() - 12)%24)
+  const fullQuery = getFromClauses() + '-is:reply -is:retweet';
+  console.log("Query length: " + fullQuery.length)
+  let params = {
+    start_time: yesterday.toISOString(),
+    max_results: 10,
+    tweet: {
+      fields: 'public_metrics'
+    },
+    query: fullQuery
+  };
+ 
+  const {meta, data} = await getClient.get('tweets/search/recent' , params);
+  console.log(meta)
+  if(meta.result_count > 0) {
+    const bestTweetId = getBestTweet(data);
+    retweetBestTweet(bestTweetId);
+  }
+}
 
-  
-  const bestTweetId = getBestTweet(data);
-  console.log("Best tweet id is:" + bestTweetId);
+async function retweetBestTweet(retweet) {
+  try {
+    const {data} = await postClient.post('statuses/retweet/'+ retweet);
+  } catch(err) {
+    console.log(err); 
+  }
 }
 
 main();
